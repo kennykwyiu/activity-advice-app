bd7f34fd43bb7114eb82c06938be59cd831c51c9
kenny <kenny.kwyiu@gmail.com>
2023-11-22 12:30:58 +0800

    22 Nov 2023 Improved SimpleRedisLock - validate the lock label before release lock by using Lua script

--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,8 +1,11 @@
 package com.hmdp.utils;
 
 import cn.hutool.core.lang.UUID;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 public class SimpleRedisLock implements ILock {
@@ -10,6 +13,13 @@ public class SimpleRedisLock implements ILock {
     private StringRedisTemplate stringRedisTemplate;
     private static final String KEY_PREFIX = "lock:";
     private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+
+    static {
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
 
     public SimpleRedisLock(String name, StringRedisTemplate stringRedisTemplate) {
         this.name = name;
@@ -26,14 +36,24 @@ public class SimpleRedisLock implements ILock {
 
     @Override
     public void unlock() {
-        String threadId = ID_PREFIX + Thread.currentThread().getId();
-        // get thread label in lock
-        String idInLock = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
-
-        // validate is it same with currentThread
-        if (threadId.equals(idInLock)) {
-            stringRedisTemplate.delete(KEY_PREFIX + name);
-        }
-
+        // use Lua script
+        stringRedisTemplate.execute(
+                UNLOCK_SCRIPT,
+                Collections.singletonList(KEY_PREFIX + name),
+                ID_PREFIX + Thread.currentThread().getId()
+                );
     }
+
+
+//    @Override
+//    public void unlock() {
+//        String threadId = ID_PREFIX + Thread.currentThread().getId();
+//        // get thread label in lock
+//        String idInLock = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
+//
+//        // validate is it same with currentThread
+//        if (threadId.equals(idInLock)) {
+//            stringRedisTemplate.delete(KEY_PREFIX + name);
+//        }
+//    }
 }
